54008734bcb3d606741296b3d1b612050beeca57
LinHuanHuang <1041349752@qq.com>
2016-03-04 15:01:51 +0800

    添加textview

--- app/src/main/java/com/qianfen/killer/gitdemo/MainActivity.java
+++ app/src/main/java/com/qianfen/killer/gitdemo/MainActivity.java
@@ -3,6 +3,7 @@ package com.qianfen.killer.gitdemo;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 
 
 /**
@@ -19,6 +20,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void btnClick(View view){
+
+        Button button = new Button(this);
 
     }
 }
